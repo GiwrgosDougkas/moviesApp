9a28bd1c38bf6229e3fb70eac2e67354fd36ddf9
Doug <barkaniza@hotmail.com>
2024-02-06 22:46:55 +0200

    create html,css structure

--- mainApp/admin.py
+++ mainApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from mainApp.models import Movie
+
 # Register your models here.
+admin.site.register(Movie)
+
--- mainApp/views.py
+++ mainApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from mainApp.models import Movie
+
+
 # Create your views here.
+
+def post_list(request):
+    movies = Movie.objects.all()
+    return render(request, 'mainApp/post_list.html', {'movies':movies})
