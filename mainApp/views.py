e8b75f2816a1f086a691811d8f5f499afd57e212
Doug <barkaniza@hotmail.com>
2024-02-07 23:02:43 +0200

    search implementation.

--- mainApp/views.py
+++ mainApp/views.py
@@ -6,8 +6,10 @@ from mainApp.models import Movie
 # Create your views here.
 
 def post_list(request):
+    renderSearch = True
     movies = Movie.objects.all()
-    return render(request, 'mainApp/post_list.html', {'movies':movies})
+    return render(request, 'mainApp/post_list.html', {'movies': movies, 'renderSearch': renderSearch})
+
 
 def about_us(request):
     return render(request, 'mainApp/about_us.html')
